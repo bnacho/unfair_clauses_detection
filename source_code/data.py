6eb7b6fa6d4a51c2c40a22691a72b249735870ae
KMS <mose712@tukora.ac.kr>
2026-01-22 06:56:25 +0900

    add load_train function

--- source_code/data.py
+++ source_code/data.py
@@ -3,43 +3,24 @@ import os
 import json
 import xml.etree.ElementTree as ET
 
+def load_train():
+    folder_path = "data/Training"
+    file_names = os.listdir(folder_path)
 
-def load_data():
-    train_folder_path = "data/Training"
-    test_folder_path = "data/Validation"
+    train = []
 
-    train_file_names = os.listdir(train_folder_path)
-    test_file_names = os.listdir(test_folder_path)
-
-    train_data = []
-    test_data = []
-
-    for file_name in train_file_names:
+    for file_name in file_names:
         if file_name.endswith(".json"):
-            with open(f"{train_folder_path}/{file_name}", 'r', encoding = 'utf-8') as f:
+            with open(f"{folder_path}/{file_name}", "r", encoding = "utf-8") as f:
                 data = json.load(f)
-                train_data.append(data)
-            f.close()
-    
-    for file_name in test_file_names:
-        if file_name.endswith(".xml"):
-            with open(f"{test_folder_path}/{file_name}", 'r', encoding = 'utf-8') as f:
-                try:
-                    root = ET.fromstring(f.read())
-                    data = root.find("cn")
-                    test_data.append(data)
-                except ET.ParseError:
-                    print(f"Error parsing {file_name}")
+                train.append(data)
             f.close()
 
-    train = pd.DataFrame(train_data)
-    test = pd.DataFrame(test_data)
+    df = pd.DataFrame(train)
 
-    return train, test
+    return df
 
-train, test = load_data()
-print(train)
-print("="*50)
-print(test)
+df = load_train()
+print(df)
 
 
