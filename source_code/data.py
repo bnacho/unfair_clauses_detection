82d75c98dc54f5427c4b92042766c7bdc5ec228b
KMS <mose712@tukora.ac.kr>
2026-01-22 09:56:52 +0900

    Change test data

--- source_code/data.py
+++ source_code/data.py
@@ -1,16 +1,22 @@
+# 라이브러리 임포트
 import pandas as pd 
 import os
 import json
 import xml.etree.ElementTree as ET
 
 def load_train():
+    '''
+    학습 데이터 불러오기
+    json 형식
+    파일이 나눠져 있으므로 반복문을 통해 로드
+    '''
     folder_path = "data/Training"
-    file_names = os.listdir(folder_path)
+    file_names = os.listdir(folder_path) # 폴더 안에 파일들을 불러온다.
 
     train = []
 
     for file_name in file_names:
-        if file_name.endswith(".json"):
+        if file_name.endswith(".json"): # 끝이 .json은 파일들만 걸러낸다.
             with open(f"{folder_path}/{file_name}", "r", encoding = "utf-8") as f:
                 data = json.load(f)
                 train.append(data)
@@ -21,22 +27,22 @@ def load_train():
     return df
 
 def load_test():
+    '''
+    테스트 데이터 불러오기
+    json 형식
+    파일이 나눠져 있으므로 반복문을 통해 로드
+    '''
     folder_path = "data/Validation"
     file_names = os.listdir(folder_path)
     test = []
 
     for file_name in file_names:
-        if file_name.endswith(".xml"):
-            try:
-                tree = ET.parse(f"{folder_path}/{file_name}")
-                root = tree.getroot()
-
-                for cn in root.iter("cn"):
-                    test.append(cn.text)
-            except:
-                pass
-        
-    df = pd.DataFrame(test, columns = ["clauseArticle"])
+        if file_name.endswith(".json"):
+            with open(f"{folder_path}/{file_name}", "r", encoding = "utf-8") as f:
+                data = json.load(f)
+                test.append(data)
+            f.close()
+    df = pd.DataFrame(test)
 
     return df
 
